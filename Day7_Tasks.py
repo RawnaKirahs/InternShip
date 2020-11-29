3747a372965b951cfe8f1ee8e4c588e426c4448c
Sharik Anwar Z <61445120+RawnaKirahs@users.noreply.github.com>
2020-11-29 23:04:37 +0530

    Create Day7_Tasks.py

---
+++ Day7_Tasks.py
@@ -0,0 +1,36 @@
+TASK#1 
+  Create a python module with list and import the module in another .py file and change the value in list
+  
+import day7_task1moD
+a=64
+b=69
+c=day7_task1.add(a,b)
+d=day7_task1.sub(a,b)
+print("sum of addition:",c)
+print("sum of subtraction:",d)
+
+
+TASK#2
+  Install pandas package (try to import the package in a python file
+  
+import pandas as pd
+s = pd.Series([1, 2, 3, 4, 5])
+print(s)
+
+
+TASK#3
+  Import a module that picks random number and write a program to fetch a random number from 1 to  100 on every run
+  
+import random
+print(random.randrange(1,100))
+
+
+TASK#4
+  Import sys package and find the python path
+  
+import sys
+print (sys.path)
+
+
+TASK#5
+  A package has been successfully installed and uninstalled using pip
